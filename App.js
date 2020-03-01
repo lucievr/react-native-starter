589954056f3cec90c98016d86974339e7aae9496
lucievr <lucy.vrsovska@gmail.com>
2020-03-01 02:09:00 +0100

    Add heading and text input with a submit button

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [headingText, setHeadingText] = useState(
+    "Welcome to my first React Native app ⚛"
+  );
+  const [inputValue, setInputValue] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>{headingText}</Text>
+      <TextInput
+        value={inputValue}
+        placeholder="Type in your name"
+        style={styles.inputBox}
+        onChangeText={(text) => setInputValue(text)}
+      />
+      <Button 
+        title="Submit"
+        style={styles.button}
+        onPress={() => setHeadingText("Thank you!")} 
+      />
     </View>
   );
 }
@@ -12,8 +28,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffdd76",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
   },
+  heading: {
+    fontSize: 26,
+    padding: 10,
+    textAlign: "center",
+  },
+  inputBox: {
+    height: 30,
+    fontSize: 18,
+    margin: 30,
+  },
+  button: {
+    padding: 20,
+    margin: 30,
+  }
 });
